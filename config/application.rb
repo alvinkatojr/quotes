6ea82070faec1c0bc17ab0ca8583599e6874e611
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-01-06 16:58:48 +0300

    Requires quotes_controller

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require "rulers"
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
+require "quotes_controller"
+
 module Quotes
   class Application < Rulers::Application
   end
