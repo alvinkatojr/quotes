dc4a12a67ed6c2a2bed770efc2c9801f5c20fd2d
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-04-24 23:39:44 +0300

    Adds test_rendering action

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,12 @@ class QuotesController < Rulers::Controller
     render :a_quote, noun => :winking
   end
 
+  def test_rendering
+    @noun = 'a BlendTec blender'
+    @anothe_noun = 'a mutton, lettuce and' + 'tomato sandwich'
+    render :test_rendering
+  end
+
   def exception
     raise "It's a bad one!"
   end
